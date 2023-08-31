ee4eea4e09c0f70284663827025e0c11f0f2f45d
hacarrasco <Hugo.aCasstillo@gmail.com>
2023-08-31 17:55:43 -0600

    all changes

--- src/reducer.js
+++ src/reducer.js
@@ -13,7 +13,7 @@ export const actionTypes = {
 }
 
 
-export const getBasketTotal =( basket) => basket?.reduce((amount, item) => item.price + amount, 0);
+export const getBasketTotal = (basket) => basket?.reduce((amount, item) => item.price + amount, 0);
 
 export const reducer = (state, action) => {
     console.log(action)
